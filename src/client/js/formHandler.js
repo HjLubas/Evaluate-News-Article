c78ed6b75e827d5aa11158d6b8916869dd139b39
Harry Lubas <hlubas@hotmail.com>
2021-06-24 23:23:13 -0700

    "client <> server calls working"

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,50 @@
-function handleSubmit(event) {
-    event.preventDefault()
-
+async function handleSubmit(event) {
+    event.preventDefault();
+    const link = document.getElementById('link');
+    const subjectivity = document.getElementById('subjectivity');
+    const confidence = document.getElementById('confidence');
+    const irony = document.getElementById('irony');
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    const formText = document.getElementById('name').value;
     Client.checkForName(formText)
+    const data = { formText };
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+    console.log("::: Form Submitted :::");
+    const response = await fetch('http://localhost:8081/test', options);
+    const json = await response.json();
+    link.innerHTML = `<p><a href="` + json.url_analyzed + `">` + json.url_analyzed +`</a></p>`;
+    subjectivity.innerHTML = json.sentiments.subjectivity;
+    confidence.innerHTML = json.sentiments.confidence;
+    irony.innerHTML = json.sentiments.irony;
+    console.log(json);
+    // formText.value = "";
+    // fetch('http://localhost:8081/test', options)
+    //     .then(res => res.json())
+    //     .then(function (res) {
+    //         document.getElementById('results').innerHTML = res.message
+    //     })
+};
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
-}
+
+
 
 export { handleSubmit }
+// const inputText = document.getElementById('name').value;
+
+// const data = { inputText };
+// const options = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+// };
+// const response = await fetch('/api', options);
+// const json = await response.json();
+// console.log(json);
--- src/server/index.js
+++ src/server/index.js
@@ -2,20 +2,30 @@ const dotenv = require('dotenv');
 dotenv.config();
 console.log(`Your API key is ${process.env.API_KEY}`);
 
-var textapi = new meaningcloud({
-    application_key: process.env.API_KEY
- });
+// var textapi = new meaningcloud({
+//     application_key: process.env.API_KEY
+//  });
 
 var path = require('path')
 const express = require('express')
+const cors = require('cors');
+const fetch = require('node-fetch');
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
+app.use(cors());
 app.use(express.static('dist'))
-
+app.use(express.json());
 console.log(__dirname)
 
+
+// app.post('/api', (request, response) => {
+//     const data = request.body;
+//     // const apiData = { data };
+//     console.log(data);
+//     response.json(data);
+// })
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
@@ -26,6 +36,19 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+app.post('/test',  async(req, res) => {
+
+    const apiURL_start = 'https://api.meaningcloud.com/sentiment-2.1?key=';
+    const textapi = process.env.API_KEY;
+    const data = req.body.formText;
+    const apiURL_mid = '&of=json&lang=en&url=';
+    const response = await fetch(apiURL_start + textapi + apiURL_mid + data);
+    const meaningCloud_data = await response.json();
+    console.log(meaningCloud_data);
+    newEntry = {
+        status: 'succesful',
+        url_analyzed: data,
+        sentiments: meaningCloud_data
+    }
+    res.json(newEntry);
 })
